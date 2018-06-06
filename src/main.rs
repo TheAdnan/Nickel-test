d01d66a9218d588cda943ec23545ba95d4fb82d1
TheAdnan <adnankicin92@gmail.com>
2018-06-06 14:07:43 +0200

    post req

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 #[macro_use] extern crate nickel;
-use nickel::{Nickel, HttpRouter};
+extern crate rustc_serialize;
+use nickel::{Nickel, HttpRouter, JsonBody};
 
+#[derive(RustcDecodable, RustcEncodable)]
+struct User{
+    name: String,
+    username: String,
+    mail: String
+}
 fn main() {
     let mut server = Nickel::new();
 
@@ -9,6 +16,12 @@ fn main() {
     ));
     server.get("/user/:id", middleware! { |request|
         format!("This is user: {:?}", request.param("id").unwrap())
+    });
+
+    server.post("/user/view", middleware!{
+        |req, res|
+        let user = req.json_as::<User>().unwrap();
+        format!("Oh hi {}. This is your username {} and email {}", user.name, user.username, user.mail);
     });
 
     server.listen("127.0.0.1:1992");
