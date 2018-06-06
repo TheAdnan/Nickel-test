5c034846aa3791b1f07c83cc5094ee9cd9da1c73
TheAdnan <adnankicin92@gmail.com>
2018-06-06 14:42:44 +0200

    post request with json data

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,16 @@ fn main() {
     let mut server = Nickel::new();
 
     server.get("/kicin", middleware!(
-        "/kicin route"
+        "<h1>Welcome!</h1><div><p>My name is <a href='http://theadnan.github.io' target='_blank'>Adnan</a></p></div>"
     ));
     server.get("/user/:id", middleware! { |request|
         format!("This is user: {:?}", request.param("id").unwrap())
     });
 
-    server.post("/user/view", middleware!{
+    server.post("/kicin", middleware!{
         |req, res|
         let user = req.json_as::<User>().unwrap();
+        //format!("{:#?}", req.json_as::<User>().unwrap())
         format!("Oh hi {}. This is your username {} and email {}", user.name, user.username, user.mail);
     });
 
