6fc33c46d2e72084e02f4c51c318374de8ac4e7f
TheAdnan <adnankicin92@gmail.com>
2018-06-06 13:44:45 +0200

    routing in nickel :cat2:

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 #[macro_use] extern crate nickel;
 
-use nickel::Nickel;
+use nickel::{Nickel, HttpRouter};
 
 fn main() {
     let mut server = Nickel::new();
 
-    server.utilize(router!{
-    get "**" => |_req, _res|{
-        "Welcome to Rust Web!"
-    }
+    server.get("/kicin", middleware!(
+        "/kicin route"
+    ));
+    server.get("/user/:id", middleware! { |request|
+        format!("This is user: {:?}", request.param("id").unwrap())
     });
 
     server.listen("127.0.0.1:1992");
